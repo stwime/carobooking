a45b464de6f183be6c0ce02b12a26934ec62330d
stwime <stwime@gmail.com>
2021-04-26 21:51:34 +0200

    Added cookie consent banner

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Head from 'next/head'
+import CookieConsent, { Cookies } from "react-cookie-consent";
+
 
 import '../global.css';
 
@@ -15,6 +17,23 @@ export default function App({ Component, pageProps }) {
         <meta name="robots" content="index, follow"></meta>
       </Head>
       <Component {...pageProps} />
+      <CookieConsent
+        location="bottom"
+        disableStyles={true}
+        buttonText="Agree!"
+        cookieName="hasConsentedToCookies"
+        containerClasses="flex h-auto md:h-12 bg-gray-300 sticky bottom-0 "
+        sameSite="strict"
+        contentClasses="flex items-center m-4"
+        buttonClasses="m-2 bg-white rounded p-2 py-1 max-h-12 shadow hover:text-white hover:bg-gray-800 transition duration-150"
+        buttonWrapperClasses="flex align-middle"
+        expires={150}
+        onAccept={() => {
+         // Load google analytics
+        }}
+      >
+        We use cookies for analytics and for an enhanced user experience, such as embedding content from other websites.
+      </CookieConsent>
     </>
   );
 }
