520449ec1119fabcd8fe19b4c549a5d9aa66b04b
stwime <stwime@gmail.com>
2021-04-30 12:06:13 +0200

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -224,9 +224,9 @@ const Home = () => (
       <div className="w-full mb-4">
         <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
       </div>
-      <h3 className="my-4 text-3xl leading-tight">
+      <h2 className="my-4 text-3xl leading-tight">
         Book your first lesson and start learning today!
-      </h3>
+      </h2>
       <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
         <a href="/book">Book now!</a>
       </button>
