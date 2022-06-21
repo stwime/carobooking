1fa576191064cfcb4c8e7a84abc923c3877e8ca1
stwime <stwime@gmail.com>
2022-06-21 19:10:54 +0200

    Fixed booking link

--- components/calendlycontainer.jsx
+++ components/calendlycontainer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function calendlycontainer() {
   return (
       <iframe
-        src="https://calendly.com/carokowanzspanish"
+        src="https://calendly.com/carokowanz"
         width="100%"
         height="100%"
         frameBorder={0}></iframe>
